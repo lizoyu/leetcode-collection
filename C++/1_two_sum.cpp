abe98cb59a5c33c65d26382654f0eb07a351272d
Chungyuen Li <lizoyu1@gmail.com>
2018-10-09 23:01:18 +0800

    minor fix

--- C++/1_two_sum.cpp
+++ C++/1_two_sum.cpp
@@ -6,7 +6,7 @@ using std::unordered_map;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> numskeep;
+        std::unordered_map<int, int> numskeep;
         for(int i = 0; i != nums.size(); ++i){
             auto iter = numskeep.find(nums[i]);
             if(iter != numskeep.end()){
