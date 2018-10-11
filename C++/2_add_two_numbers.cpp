a18868c4b49a9be963b462616209919e4a9d9b9a
Chungyuen Li <lizoyu1@gmail.com>
2018-10-11 20:56:49 +0800

    use pure pointer

--- C++/2_add_two_numbers.cpp
+++ C++/2_add_two_numbers.cpp
@@ -5,6 +5,10 @@ struct ListNode {
     ListNode(int x) : val(x), next(nullptr) {}
 };
 
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
@@ -18,23 +22,24 @@ public:
         
         // use a int for carry flag, a cycling node and a counter for ending
         int add = 0, count = 0;
-        ListNode node(0);
-        ListNode* endNode = &node;
+        ListNode *endNode = new ListNode(0), *head = new ListNode(0), *p = head;
         endNode->next = endNode;
-        ListNode* head = l1;
-        
+
         // when both numbers are not exhausted
         while (count < 2) {
             int sum = l1->val + l2->val + add;
             add = sum / 10;    // update carry flag
-            l1->val = sum % 10;
-            if (!l1->next){
+            p->next = new ListNode(sum % 10);
+            p = p->next;
+
+            // if l1 or/and l2 ends, replace with the cycling node, and count
+            if (!l1->next) {
                 l1 = endNode;
                 count += 1;
             }
             else
                 l1 = l1->next;
-            if (!l2->next){
+            if (!l2->next) {
                 l2 = endNode;
                 count += 1;
             }
@@ -43,12 +48,25 @@ public:
         }
         
         // handle the carry flag
-        if (add) {
-            endNode->val = add;
-            endNode->next = nullptr;
-            l1->next = endNode;
-        }
+        if (add)
+            p->next = new ListNode(add);
         
-        return head;
+        return head->next;
     }
 };
+
+// driver
+int main(){
+    ListNode *l1 = new ListNode(6), *l2 = new ListNode(5);
+    Solution s;
+
+    ListNode* l3 = s.addTwoNumbers(l1, l2);
+
+    while(l3) {
+        cout << l3->val << endl;
+        l3 = l3->next;
+    }
+
+    system("pause");
+    return 0;
+}
