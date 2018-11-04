8de4a3f795d0abda11c8c1e2e0ba4cb4e89f175d
Chungyuen Li <lizoyu1@gmail.com>
2018-11-04 12:49:19 +0800

    add comments

--- C++/4_median_of_two_sorted_arrays.cpp
+++ C++/4_median_of_two_sorted_arrays.cpp
@@ -13,11 +13,14 @@ public:
         if (totalLength % 2)
             numOfMedians = 1;
         else
+            // two numbers in middle are needed to compute median
             numOfMedians = 2;
         
         // loop to cut unnecessary half of a array
+        // i, j: head and tail of arrays
         int i1 = 0, j1 = nums1.size() - 1, i2 = 0, j2 = nums2.size() - 1, medianIdx = totalLength / 2;
         while (true) {
+            // compute the length of remaining array
             int len1 = j1 - i1 + 1, len2 = j2 - i2 + 1;
 
             // ending condition: only 1 or 2 numbers left
@@ -66,35 +69,3 @@ int main() {
     system("pause");
     return 0;
 }
-
-/***
-*def findMedianSortedArrays(self, A, B):
-*    l = len(A) + len(B)
-*    if l % 2 == 1:
-*        return self.kth(A, B, l // 2)
-*    else:
-*        return (self.kth(A, B, l // 2) + self.kth(A, B, l // 2 - 1)) / 2.   
-*    
-*def kth(self, a, b, k):
-*    if not a:
-*        return b[k]
-*    if not b:
-*        return a[k]
-*    ia, ib = len(a) // 2 , len(b) // 2
-*    ma, mb = a[ia], b[ib]
-*    
-*    # when k is bigger than the sum of a and b's median indices 
-*    if ia + ib < k:
-*        # if a's median is bigger than b's, b's first half doesn't include k
-*        if ma > mb:
-*            return self.kth(a, b[ib + 1:], k - ib - 1)
-*        else:
-*            return self.kth(a[ia + 1:], b, k - ia - 1)
-*    # when k is smaller than the sum of a and b's indices
-*    else:
-*        # if a's median is bigger than b's, a's second half doesn't include k
-*        if ma > mb:
-*            return self.kth(a[:ia], b, k)
-*        else:
-*            return self.kth(a, b[:ib], k)
-***/
